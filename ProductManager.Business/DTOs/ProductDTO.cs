004f09f6739407f60cb0cf335b5348523de192b8
Sêneca Brito <limaseneca@gmail.com>
2026-03-30 14:38:48 -0300

    refactor(ProductDTO): expõe ID para o usuário e serializa corretamente como deve exibir os campos

--- ProductManager.Business/DTOs/ProductDTO.cs
+++ ProductManager.Business/DTOs/ProductDTO.cs
@@ -5,13 +5,16 @@ namespace ProductManager.Business.DTOs
 {
     public record ProductDTO
     {
-        [JsonIgnore]
+        [JsonPropertyName("code")]
         public Guid Id { get; init; }
 
+        [JsonPropertyName("name")]
         public string Name { get; init; } =  string.Empty;
 
+        [JsonPropertyName("price")]
         public decimal Price { get; init; }
 
+        [JsonPropertyName("quantity")]
         public int Quantity { get; init; }
 
         [JsonConverter(typeof(JsonStringEnumConverter))]
