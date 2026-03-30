4b65e9fec6b27e86a368c6913debd5c0923e50bb
Sêneca Brito <limaseneca@gmail.com>
2026-03-30 14:56:35 -0300

    refactor(ProductDTO): expõe atributos com nome padrão

--- ProductManager.Business/DTOs/ProductDTO.cs
+++ ProductManager.Business/DTOs/ProductDTO.cs
@@ -5,16 +5,12 @@ namespace ProductManager.Business.DTOs
 {
     public record ProductDTO
     {
-        [JsonPropertyName("code")]
         public Guid Id { get; init; }
 
-        [JsonPropertyName("name")]
         public string Name { get; init; } =  string.Empty;
 
-        [JsonPropertyName("price")]
         public decimal Price { get; init; }
 
-        [JsonPropertyName("quantity")]
         public int Quantity { get; init; }
 
         [JsonConverter(typeof(JsonStringEnumConverter))]
